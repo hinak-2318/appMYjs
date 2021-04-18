c29bbed4e6fff35d9a3728d7cf39eb41b1c9a180
hinak-2318 <hinasarfaraz@gmail.com>
2021-04-18 10:34:41 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,299 @@
+// //           CHAPTER (ALERT)
+// //           Question Answer
+// //  Q1. Write a script to greet your website visitor using JS alert box.
+//         alert("Hello World");
+
+// // Q2. Write a script to display following message on your web  page: 
+//         alert("Error! Please enter a valid password.")
+
+// // Q3. Write a script to display following message on your web page: (Hint : Use line break)
+//          alert("Welcome to JS land.... \n happy coding!");
+
+
+//  // Q4. Write a script to display following messages in sequence:
+//        alert("Welcome to JS land...")
+//        alert("Happy coding!") 
+//        alert("Prevent this page from creating additional dialogs.")
+
+
+// // //  Q5. Generate the following message through browser’s developer console:
+//            alert("Hello... I can run JS through my web browser's console ")
+
+
+        
+    
+         
+
+// // //           CHAPTER #2:  (VARIABLES FOR STRINGS)
+// // //                 Question Answer
+
+// // //   Q1. Declare a variable called username.
+//            var username = "Hina";
+//            alert(username);
+
+
+// //      Q2. Declare a variable called myName & assign to it a string that represents your Full Name.
+//             var myname = "Hina";
+//             var lastname = "Srfaraz";
+//             var fullname = myname + lastname;
+//             alert(fullname);     
+
+ 
+// //      Question # 3:
+//             var message = "hello";
+//             var friend = "class";
+//             var convay = message + friend;
+//             alert(convay);
+
+
+// //      Question # 4 
+//              var myname = "Hina Sarfaraz";
+//              var age = "32";
+//              var certified = "Certified Mobile Application Development"
+//              alert (myname);
+//              alert (age);
+//              alert (certified);
+
+
+// //      Question # 5:
+//             alert("pizza \n pizz \n piz \n pi \n p");
+
+// //      Question # 6: 
+//               var text = "My Email is";
+//               var email = "hinasarfaraz0123@gmail";
+//               var myemail = text + email;
+//               alert(myemail);
+// //      Question # 7: 
+//               alert("I am trying to learn from the book" + "A smarter way to learn javascript")
+
+
+// //      Question # 8: 
+// //      <script>
+// //     document.write("yeah i can write HTML content through javscript")
+
+
+// //      Question # 9: 
+//            alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+
+
+
+
+
+// //     Chapter # 3:  VARIABLES FOR NUMBERS 
+// //         Question # 1: 
+//             var me = "I am";
+//             var age = "32";
+//             var text = "years old";
+//             var year = me + age + text
+//             alert(year);
+
+// //         Question # 2:
+//              var visit = 'you have visited this site';
+//              var num = "14"
+//              var message = 'times';
+//              var counted = visit + num + message ;
+//              alert(counted);
+             
+// //         Question # 4: 
+//              <script> document.write("<h4>my birthday month is 1989 <br> Datatype of my ddeclared variable is number</h4>");</script>
+
+
+
+
+//            // CHAPTER#4 (LEGAL AND ILLEGAL)
+      // QUESTIONS ANSWER
+      //  question no #1 
+    //   var variable1 = "my first year";
+    //   var variable2 = "result is ....";
+    //   var variable3 = 78;
+    //   alert("variable1 + variable2 + variable3");
+        //   question no #2
+
+   // legal ways
+   // var myName;
+  //  var sana;
+  //  var t@smia;
+  //  var  khadija;
+  //  var  _nafesa;
+  //illegal ways
+  //  var %sayra;
+  //  var  4a; 
+  //  var  4b;  
+  //  var  @st;  
+  //  var  &we;  
+  
+
+ //   question no #3
+
+
+
+   // CHAPTER#5 (MATH EXPRESSIONS)
+  //        question #1
+        //   var a = 3;
+        //   var b = 5;
+        //   var c = a + b;
+        //   document.write("sum of 3 and 5 is " +  c);
+          
+ //            question #2
+//  subtractction
+//  var a = 3;
+//  var b = 5;
+//  var c = a - b;
+//  document.write("<br>sum of 3 and 5 is" +  c);
+// multiplication
+// //  var a = 3;
+//  var b = 5;
+//  var c = a * b;
+//  document.write("<br>sum of 3 and 5 is" +  c);
+// division
+//  var a = 3;
+//  var b = 5;
+//  var c = a / b;
+//  document.write("<br>sum of 3 and 5 is" +  c);
+
+//            question #3
+ 
+//   part (a)
+//   var value;
+
+// //   part (b)
+//    document.write("value after variable declaretion is " + value + "<br>");
+// //   part (c)
+
+//   value = 8;
+
+// //   part (d)
+  
+//   document.write("initial value of variable is " + value + "<br>");
+
+
+// //   part (e)
+  
+//     value++;
+
+// //     part (f)
+
+//     document.write("value after increment is "+ vlue +"<br>");
+
+// //     part (g)
+
+//     value = value+7;
+
+// //     part (h)
+
+//     document.write("value after addition is " +value + "<br>");
+
+// //     part (i)
+    
+//     value--;
+
+// //     part (j)
+
+//     document.write("value after decrement is " + value + "<br>");
+
+
+// //     part (k)
+// //     part (l)
+
+
+//     value = value%value;
+//     document.write("value is " + value);
+
+
+// //            question #4
+//    var ticketPrice = 600;
+//    var total = ticketPrice*5;
+//    document.write("total cost of buying 5 tickets to a movie is " + "PKR");
+       
+
+// //            question #5
+
+// document.write("<h1>Table of four <br> </h1>");
+// //  without using loop
+// document.write(4 + " x " + 1 + " = " + 4*1 + "<br>");
+// document.write(4 + " x " + 2 + " = " + 4*2 + "<br>");
+// document.write(4 + " x " + 3 + " = " + 4*3 + "<br>");
+// document.write(4 + " x " + 4 + " = " + 4*4 + "<br>");
+// document.write(4 + " x " + 5 + " = " + 4*5 + "<br>");
+// document.write(4 + " x " + 6 + " = " + 4*6 + "<br>");
+// document.write(4 + " x " + 7 + " = " + 4*7 + "<br>");
+// document.write(4 + " x " + 8 + " = " + 4*8 + "<br>");
+// document.write(4 + " x " + 9 + " = " + 4*9 + "<br>");
+// document.write(4 + " x " + 10 + " = " + 4*10 +"<br>");
+
+
+//            question #6
+//  part (a)
+// part (b)
+//    var celsius = prompt("enter temp in celsius");
+//    var Faranheit = 9/5*celsius+32;
+//   alert(Faranheit);
+
+//  part (c)
+// part (d)
+//    var Faranheit = 12;
+//    var celsius = 5/9 * (Faranheit -32);
+//    alert(celsius);
+
+
+//            question #7
+// part (a)
+// part (b)
+// part (c)
+// part (d)
+// part (e)
+
+// var item1 = 100;
+// var item2 = 500;
+// var orderedItem1 = 3;
+// var orderedItem2 = 1;
+// var total = item1 + 3 + item2 + shippingCharges; 
+// var receipt = "price of item  1 is" + item1 + "<br> quantity of item 1 is " + orderedItem1 +"<br> price of item 2 is" + item2 + "<br> quantity of item 1 is" + orderedItem2 + " <br>shippingCharges" ;
+
+// Document.write(receipt);
+
+//            question #8
+//    var totalMarks = prompt("enter total marks");
+//    var marksObtained = prompt("enter obtained marks");
+//    var percentage = marksObtained/totalMarks*100;
+//    document.write("your percentage is" +percentage + "%");
+
+   //            question #9
+//    var PKR = 10*104.8 + 25*28;
+
+//     document.write(PKR);
+
+ //            question #10
+
+ // part (a)
+ // part (b)
+ // part (c)
+
+//    var arthmetic = ((4+5)*10)/2;
+//    document.write(arthmetic);
+
+ //            question #11
+// part (a)
+// part (b)
+// part (c)
+
+//    var currentYear = 2021;
+//    var birthYear = +prompt("enter your birth year");
+//    var age = currentYear-birthYear;
+//    var approxAge = currentYear-birthYear-1
+//    document.write("your age is approximately" + " or " + approxAge + "years");
+
+
+ //            question #12
+//  part (a)
+
+//   var radius = +prompt("enter radius");
+
+//  part (b)
+//    var circumference = 2*3.142*radius;
+//    document.write("the circumference is " + circumference);
+
+//    Calculate the area based on the radius, and output “The area is NN”. (Hint : Area of a circle = π r2, π = 3.142)
+//  var area = 3.142*radius^2;
+//  document.write("the area of circle is " + area);
+
